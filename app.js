275d7f99cb2f2e97e4f2033b13069644be3e9715
程凌云 <chenglyclyccc@gmail.com>
2026-07-14 15:49:10 +0800

    fix: auto apply mini program updates

--- app.js
+++ app.js
@@ -55,12 +55,16 @@ App({
     const updateManager = wx.getUpdateManager();
     updateManager.onCheckForUpdate(() => {});
     updateManager.onUpdateReady(() => {
+      wx.showToast({ title: '新版本已更新', icon: 'none', duration: 800 });
+      setTimeout(() => {
+        updateManager.applyUpdate();
+      }, 800);
+    });
+    updateManager.onUpdateFailed(() => {
       wx.showModal({
-        title: '更新提示',
-        content: '新版本已经准备好，是否重启应用？',
-        success(res) {
-          if (res.confirm) updateManager.applyUpdate();
-        },
+        title: '更新失败',
+        content: '新版本下载失败，请退出小程序后重新打开。',
+        showCancel: false,
       });
     });
 
