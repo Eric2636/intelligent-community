5b60ef8a11c0ee165fd71e646deb3c57e0d3177d
程凌云 <chenglyclyccc@gmail.com>
2026-08-02 11:17:40 +0800

    feat: 版本大更新

--- miniprogram_npm/tdesign-miniprogram/action-sheet/type.d.ts
+++ miniprogram_npm/tdesign-miniprogram/action-sheet/type.d.ts
@@ -41,19 +41,18 @@ export interface TdActionSheetProps {
         type: BooleanConstructor;
         value?: boolean;
     };
-    visible: {
+    visible?: {
         type: BooleanConstructor;
         value?: boolean;
-        required?: boolean;
     };
-    defaultVisible: {
+    defaultVisible?: {
         type: BooleanConstructor;
         value?: boolean;
-        required?: boolean;
     };
 }
 export interface ActionSheetItem {
     label: string;
+    description?: string;
     color?: string;
     disabled?: boolean;
     icon?: string;
